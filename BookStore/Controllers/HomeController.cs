87e9a5dfe5f1bd9a70cb944ccff17d5867f24ccf
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-09-09 08:48:45 -0300

    Estudo do funcionamento do padrão MVC

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
---
+++ BookStore/Controllers/TesteController.cs
@@ -0,0 +1,51 @@
+﻿using BookStore.Domain;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+
+    [RoutePrefix("testes")]
+    //[Route("{action=Dados}")]
+    public class TesteController : Controller
+    {
+        
+        public ViewResult Dados(int Id)
+        {
+            var autor = new Author { Id = 0, Name = "Lucas" };
+
+            ViewBag.Category = "Produtos de Limpeza";
+            ViewData["Category"] = "Produtos de Informática";
+            TempData["Category"] = "Produtos de Escritório";
+            Session["Category"] = "Móveis";
+
+            return View(autor);
+        }
+
+        public string Index(int Id)
+        {
+            return $"Index do {Id}";
+        }
+
+        public JsonResult UmaAction(int? id, string nome)
+        {
+            var autor = new Author { Id = 0, Name = nome };
+
+            return Json(autor);
+        }
+
+        [HttpPost]
+        [ActionName("Autor")]
+        public JsonResult ActionDois(Author author)
+        {
+            return Json(author);
+        }
+
+        [Route("minharota/{id}")]
+       // [Route("~/minharota/{id}")]
+        public string MinhaAction(int id)
+        {
+            return "Ok! cheguei na rota.";
+        }
+
+    }
+}
